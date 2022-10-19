c252e074fa73957abee3a2bf802b2a99f2a2b0c3
LokeshVishwakarma <lokeshvishwakarma2611@gmail.com>
2022-10-19 16:58:26 +0530

    ready for first deplotment

--- src/helper/socket.js
+++ src/helper/socket.js
@@ -2,4 +2,7 @@ import { io } from "socket.io-client";
 import { initSocketOptions } from "../config";
 
 export const initSocket = async () =>
-  io(process.env.REACT_APP_BACKEND_URL, initSocketOptions);
+  io(
+    process.env.REACT_APP_BACKEND_URL || "http://localhost:5000",
+    initSocketOptions
+  );
