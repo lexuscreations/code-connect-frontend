8918e877a19b0f16f877210c096939dabf78a705
LokeshVishwakarma <72046165+lexuscreations@users.noreply.github.com>
2022-12-14 00:25:33 +0530

    UpdateBackendServerUrl
    from heroku to onrender - change backend host url

--- src/helper/socket.js
+++ src/helper/socket.js
@@ -4,6 +4,6 @@ import { initSocketOptions } from "../config";
 export const initSocket = async () =>
   io(
     process.env.REACT_APP_BACKEND_URL ||
-      "https://lexus-codeconnect.herokuapp.com",
+      "https://lexus-codeconnect.onrender.com",
     initSocketOptions
   );
